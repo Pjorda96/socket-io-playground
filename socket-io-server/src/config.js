0bd4a421ce8f179d2ed3cdf1b69a68f5bb8a306e
Pablo Jorda <pjorda96@gmail.com>
2021-06-27 10:52:42 +0200

    server start

---
+++ socket-io-server/src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  port: process.env.Port || 5000,
+  // db: {
+  //   url: 'mongodb://localhost/nodejsapi', // TODO: change
+  //   mongoConfig: { useNewUrlParser: true, useUnifiedTopology: true },
+  // },
+  // secretToken: 'secretkeytoken', // TODO: change
+  // tokenDuration: 8 // TODO: change (in hours)
+}
